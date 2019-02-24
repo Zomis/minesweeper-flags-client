b0e548409f6ad4cfa6be308f80d054160f162650
Simon Forsberg <zomis2k@hotmail.com>
2019-02-24 22:22:40 +0100

    Add GameView to about page for testing purposes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about fill-height">
+    <div class="resizable">
+      <GameView :game="game" class="minigame" :horizontal="false" />
+    </div>
+    <div class="resizable horizontal">
+      <GameView :game="game" class="minigame" :horizontal="true" />
+    </div>
   </div>
 </template>
+<script>
+import GameView from "./games/GameView";
+import gameTools from "./games/gameTools";
+
+export default {
+  name: "About",
+  components: { GameView },
+  data() {
+    let game = gameTools.createGame({ gameId: 42, yourIndex: 0 });
+    gameTools.addPlayers(game, ["Player1", "Player2"]);
+    return {
+      game: game
+    };
+  }
+};
+</script>
+<style scoped>
+.resizable {
+  width: 70vmin;
+  height: 70vmin;
+  resize: both;
+  overflow: auto;
+  background-color: #ddf9fd;
+  margin: 10px;
+}
+.resizable.horizontal {
+  width: 70vmax;
+  height: 70vmax;
+}
+</style>
