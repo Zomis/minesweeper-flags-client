0d81bd24608af32d4a65e14e1795453d230ae595
Simon Forsberg <zomis2k@hotmail.com>
2019-02-26 16:59:02 +0100

    If master branch then deploy to root

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+let branch = process.env.VUE_APP_GIT_BRANCH;
+if (branch === "master") {
+  branch = "";
+}
+
 module.exports = {
-  publicPath: "/" + process.env.VUE_APP_GIT_BRANCH
+  publicPath: "/" + branch
 };
