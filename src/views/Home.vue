b560fac02ef3de335ca868f7e680fbb2326bf9a0
Christopher Sumnicht <csumnicht@berkeley.edu>
2019-03-04 09:34:19 -0800

    Added another space, probably should at some point refactor to do better vertical alignment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,7 +22,7 @@
             :src="require('@/assets/logos/' + provider + '.svg')"
             @click="authenticate(provider)"
           />
-          &nbsp;Log in with {{ provider }}
+          &nbsp;&nbsp;Log in with {{ provider }}
         </v-btn>
       </v-layout>
       <v-layout justify-center row>
