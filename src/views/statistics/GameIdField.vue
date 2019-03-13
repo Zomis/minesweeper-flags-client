ed639f1d255076917d92ee1297e3b2da689a9663
Simon Forsberg <zomis2k@hotmail.com>
2019-03-13 16:41:41 +0100

    Make gameId a link to game in statistics

--- src/views/statistics/GameIdField.vue
+++ src/views/statistics/GameIdField.vue
@@ -1,5 +1,7 @@
 <template>
-  <span>{{ gameId }}</span>
+  <router-link :to="{ name: 'game', params: { gameId: gameId } }">
+    <span>{{ gameId }}</span>
+  </router-link>
 </template>
 <script>
 export default {
