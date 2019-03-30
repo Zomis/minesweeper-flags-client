3f63ea25cb70389a370705c9ee8e128e801dc2ab
Simon Forsberg <zomis2k@hotmail.com>
2019-03-31 00:35:50 +0100

    Reduce padding of GameView by removing some containers

--- src/views/games/GameView.vue
+++ src/views/games/GameView.vue
@@ -3,38 +3,39 @@
     <v-container fluid>
       <v-layout map-flex align-center justify-center column fill-height>
         <h1>Game {{ game.gameId }}</h1>
-        <v-container fluid>
-          <v-layout
-            map-flex
-            align-space-around
-            justify-center
-            :fill-height="$vuetify.breakpoint.lgAndUp"
-            :column="$vuetify.breakpoint.mdAndDown"
-          >
-            <v-flex xs12 lg4 xl6>
-              <v-container fluid>
-                <v-layout
-                  align-center
-                  justify-center
-                  :column="$vuetify.breakpoint.lgAndUp"
-                  :fill-height="$vuetify.breakpoint.lgAndUp"
-                  style="height: 100%"
-                >
-                  <PlayerView :playerData="game.playerData[0]" />
-                  <h1 class="mines-remaining">{{ game.map.minesLeft }}</h1>
-                  <PlayerView :playerData="game.playerData[1]" />
-                </v-layout>
-              </v-container>
-            </v-flex>
-            <v-flex xs12 lg8 xl6 map-flex>
-              <MapView
-                :onClick="onClick"
-                :game="game"
-                :highlightWeapon="highlightWeapon"
-              />
-            </v-flex>
-          </v-layout>
-        </v-container>
+        <v-layout
+          class="fill-height"
+          map-flex
+          align-space-around
+          justify-center
+          :fill-height="$vuetify.breakpoint.lgAndUp"
+          :column="$vuetify.breakpoint.mdAndDown"
+        >
+          <v-flex xs12 lg3 xl4>
+            <v-layout
+              class="fill-height"
+              align-center
+              justify-center
+              :column="$vuetify.breakpoint.lgAndUp"
+              :fill-height="$vuetify.breakpoint.lgAndUp"
+              style="height: 100%"
+            >
+              <PlayerView :playerData="game.playerData[0]" />
+              <h1 class="mines-remaining">{{ game.map.minesLeft }}</h1>
+              <PlayerView :playerData="game.playerData[1]" />
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 lg6 xl6 map-flex>
+            <MapView
+              :onClick="onClick"
+              :game="game"
+              :highlightWeapon="highlightWeapon"
+            />
+          </v-flex>
+          <v-flex xs12 lg3 xl2>
+            In-game chat
+          </v-flex>
+        </v-layout>
       </v-layout>
     </v-container>
   </div>
@@ -75,6 +76,7 @@ export default {
   height: 100%;
 }
 .map-flex {
+  width: 100%;
   height: 100%;
 }
 </style>
